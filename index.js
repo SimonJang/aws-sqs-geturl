e43395d4e537ab6bddaffddbbf16d43d99792bd2
simonjang <simon.jang.contact@gmail.com>
2017-05-01 10:41:47 +0200

    add test

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
+
+const AWS = require('aws-sdk');
+const pify = require('pify');
+const isAwsAccountId = require('is-aws-account-id');
+
+const sqs = new AWS.SQS();
+const getQueueUrl = pify(sqs.getQueueUrl.bind(sqs));
+
+module.exports = (queueName, opts) => {
+	if (typeof queueName !== 'string') {
+		return Promise.reject(new TypeError(`Expected \`queueName\` to be of type \`string\`, got \`${typeof queueName}\``));
+	}
+	if (!/^[a-z0-9_-]{1,80}$/i.test(queueName)) {
+		return Promise.reject(new TypeError('Invalid queue name'));
+	}
+	if (opts.awsAccountId && !isAwsAccountId(opts.awsAccountId)) {
+		return Promise.reject(new TypeError('Invalid AWS Account Id'));
 	}
 
-	opts = opts || {};
-
-	return input + ' & ' + (opts.postfix || 'rainbows');
+	return getQueueUrl(
+		{
+			QueueName: queueName,
+			QueueOwnerAWSAccountId: opts.awsAccountId
+		}
+	)
+	.then(data => {
+		if (!data || !data.QueueUrl) {
+			throw new TypeError(`Queue \`${queueName}\` not found`);
+		}
+		return data.QueueUrl;
+	});
 };
